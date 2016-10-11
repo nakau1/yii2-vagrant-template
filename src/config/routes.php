88e945a2d07f9f1a1abaaa12f7b46f11e828b1a7
中安佑一 <yuichi.nakayasu@gmail.com>
2016-10-12 01:42:13 +0900

    ルーティングの更新

--- src/config/routes.php
+++ src/config/routes.php
@@ -1,15 +1,5 @@
 <?php
 return [
     // DefaultController
-    ''               => 'default/index',
-    'top'            => 'default/top',
-    'tutorial'       => 'default/tutorial',
-    'guide'          => 'default/guide',
-    'first-guide'    => 'default/first-guide',
-    'help'           => 'default/help',
-    'setting'        => 'default/setting',
-    'terms'          => 'default/terms',
-    // API
-    'api/'           => 'api/default/index',
-    'api/initialize' => 'api/default/initialize',
+    '' => 'default/index',
 ];
